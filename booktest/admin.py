d84bb16f86719bd2926e05400ce954096e5e264d
强周亮 <2538096489@qq.com>
2021-05-29 17:03:22 +0800

    admin站点配置提交记录

--- booktest/admin.py
+++ booktest/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from booktest.models import BookInfo, HeroInfo
+from booktest.models import BookInfo, HeroInfo, AreaInfo
 
 
 # 后台管理相关文件
@@ -16,6 +16,35 @@ class HeroInfoAdmin(admin.ModelAdmin):
     list_display = ['id', 'hname', 'hgender', 'hcomment']
 
 
+class AreaStackedInline(admin.StackedInline):
+    """以块的形式嵌入，展示出地区的下级地区"""
+    # 写多类的名字
+    model = AreaInfo
+    # 可以追加多少个,默认三个
+    extra = 2
+
+
+class AreaInfoAdmin(admin.ModelAdmin):
+    """地区模型管理类"""
+    list_display = ['id', 'atitle', 'title', 'parent']  # 管理页面展示出来的类容
+    list_per_page = 15  # 指定每页显示15条数据
+    actions_on_bottom = True
+    actions_on_top = False
+    list_filter = ['atitle']  # 列表右侧的过虑栏
+    search_fields = ['atitle']  # 列表页上方的搜索框
+
+    #     编辑页设置
+    #     fields = ['aParent', 'atitle']  # 编辑页面字段显示顺序
+    # 分组显示 fields 和 fieldsets 只能使用一个
+    fieldsets = (
+        ('基本', {'fields': ['atitle']}),
+        ('父级', {'fields': ['aParent']})
+    )
+
+    inlines = [AreaStackedInline]
+
+
 # 注册模型类
 admin.site.register(BookInfo, BookInfoAdmin)
 admin.site.register(HeroInfo, HeroInfoAdmin)
+admin.site.register(AreaInfo, AreaInfoAdmin)
--- booktest/models.py
+++ booktest/models.py
@@ -34,7 +34,31 @@ class HeroInfo(models.Model):
     hgender = models.BooleanField(default=True)  # 英雄性别
     isDelete = models.BooleanField(default=False)  # 逻辑删除
     hcomment = models.CharField(max_length=200)  # 英雄描述信息
-    hbook = models.ForeignKey('BookInfo',on_delete=models.CASCADE)  # 英雄与图书表的关系为一对多，所以属性定义在英雄模型类中
+    hbook = models.ForeignKey('BookInfo', on_delete=models.CASCADE)  # 英雄与图书表的关系为一对多，所以属性定义在英雄模型类中
 
     class Meta:
         db_table = 'heroinfo'  # 指定模型类对应的表名
+
+
+class AreaInfo(models.Model):
+    """地址模型类"""
+    # 地区名称,verbose_name 显示的标题
+    atitle = models.CharField(verbose_name='标题', max_length=20)
+    # 自关联属性
+    aParent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.atitle
+
+    def title(self):
+        return self.atitle
+    title.admin_order_field = 'atitle'  # 给title方法添加点击排序
+    title.short_description = '地区名称'  # 指定列的标题
+
+    def parent(self):
+        """返回父地区的标题"""
+        if self.aParent is None:
+            return ''
+        else:
+            return self.aParent.atitle
+    parent.short_description = '父级地区名称'
